72149adb9901692e2e3afda73b2629a5c632eec4
Simon Surin <simon.surin@stud.uni-corvinus.hu>
2021-11-26 15:45:35 +0100

    E-mail validation test

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            /// Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
