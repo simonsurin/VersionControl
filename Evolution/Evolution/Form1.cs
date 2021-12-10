600cb20323566ffc27d9986e9d915aebb70b3a37
Simon Surin <simon.surin@stud.uni-corvinus.hu>
2021-12-10 19:20:09 +0100

    Starting population

--- Evolution/Evolution/Form1.cs
+++ Evolution/Evolution/Form1.cs
@@ -15,12 +15,23 @@ namespace Evolution
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
         }
     }
 }
