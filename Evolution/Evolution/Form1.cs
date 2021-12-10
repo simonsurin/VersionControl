fd647dc46ed8fb14d334f903737979646a696d98
Simon Surin <simon.surin@stud.uni-corvinus.hu>
2021-12-10 19:50:23 +0100

    New generation

--- Evolution/Evolution/Form1.cs
+++ Evolution/Evolution/Form1.cs
@@ -48,6 +48,22 @@ namespace Evolution
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
